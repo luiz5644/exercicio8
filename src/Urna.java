d23adfb7a902923f8f4047d7df6370399dd7295b
Luiz Carlos <luizcarslossf564@gmail.com>
2024-03-31 17:32:10 -0300

    adição de voto nulos

--- src/Urna.java
+++ src/Urna.java
@@ -8,17 +8,11 @@ public class Urna {
     int c2 = 0;
     int c3 = 0;
     int totbranco = 0;
+    int totvotosnulos = 0;
     int senha;
     int senhaadm = 564;
-    int totvotos = (c1+c2+c3)/2+1;
-    // int senhaadm = 564;
-
-
+    int totvotos = ((c1+c2+c3)/2)+1;
    
-
-
-
-
     Scanner entrada = new Scanner(System.in);
 
 
@@ -44,7 +38,7 @@ public class Urna {
         }switch (opcao) {
             case 1:
             System.out.printf("%s\n", "=-".repeat(30));
-            System.out.println("caro eleitor você pode votar nos seguintes candidatos:\n45 - Silvério Reais (PP)\n20 - Monteiro Lobato (PJ)\n12 - Elis Regina (PSJ)\nvotos nulos - digite qualquer númer");
+            System.out.println("caro eleitor você pode votar nos seguintes candidatos:\n45 - Silvério Reais (PP)\n20 - Monteiro Lobato (PJ)\n12 - Elis Regina (PSJ)\n00 - Voto em branco\nQualquer número - Voto nulo");
             System.out.printf("%s\n", "=-".repeat(30));
             votos= entrada.nextInt();
             if (votos == 45){
@@ -56,8 +50,11 @@ public class Urna {
             else if(votos == 12){
                 c3++;
             }
-            else{
+            else if(votos == 00){
                 totbranco ++;
+            }
+            else{
+                totvotosnulos ++;
             }
 
             case 2:
@@ -94,7 +91,7 @@ public class Urna {
         System.out.printf("%s\n", "-=".repeat(30));
         System.out.println("RESULTADO DA ELEIÇÃO");
         System.out.printf("%s\n", "-=".repeat(30));
-        System.out.printf("Silvério Reais (PP) teve um total de %d votos\nMonteiro Lobato (PJ) teve um total de %d votos\nElis Regina (PJS) teve um total de %d votos", c1, c2, c3);
+        System.out.printf("Silvério Reais (PP) teve um total de %d votos\nMonteiro Lobato (PJ) teve um total de %d votos\nElis Regina (PJS) teve um total de %d votos\n%d Votos brancos\n%d Votos nulos", c1, c2, c3, totbranco, totvotosnulos);
         if (c1>totvotos){
             System.out.println("\nParabéns Silvério Reais (PP) você foi o ganhador desta eleição");
             
